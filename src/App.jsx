d3d341e4fe08928bf017bbac35efe6ea782f39ac
I-M-J <149041998+I-M-J@users.noreply.github.com>
2026-03-17 11:24:39 +0600

    Countries created, api called to get all countries

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import { Suspense } from "react";
 import "./App.css";
 import Countries from "./components/Countries";
+
+const countriesPromise = fetch(
+  "https://openapi.programming-hero.com/api/all",
+).then((res) => res.json());
 
 function App() {
   return (
     <>
-      <h1>React World On The Go</h1>
+      <Suspense fallback={<p>Loading...</p>}>
+        <Countries countriesPromise={countriesPromise}></Countries>
+      </Suspense>
     </>
   );
 }
